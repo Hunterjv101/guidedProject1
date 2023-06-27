21625a5775a774c4ef3a29e6a8933cb62aec470c
Hunter Vetterkind <hvetterk@travelers.com>
2023-06-27 11:02:57 -0400

    Final touches 6/27/2023

--- guided_project_1_starter/film.js
+++ guided_project_1_starter/film.js
@@ -1,5 +1,13 @@
+// This will return one Film, do with that what you will
 async function fetchFilm(character) {
     const url = `https://swapi2.azurewebsites.net/api/films/${id}`;
+    const film = await fetch(url)
+      .then(res) => res.json();
+    return film;
+  }
+// This will return multiple characters so I have to figure that out
+async function fetchCharacter(character) {
+    const url = ` https://swapi2.azurewebsites.net/api/films/${id}/characters`;
     const film = await fetch(url)
       .then(res) => res.json();
     return film;
